a44b430add6195aecb899ca771e6b59c009e741f
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-06-24 15:00:13 -0300

    adjust and style expenses list

--- src/components/ExpensesOutput/ExpensesOutput.js
+++ src/components/ExpensesOutput/ExpensesOutput.js
@@ -16,7 +16,9 @@ export default function ExpensesOutput({expenses, expensesPeriod}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   },
   text: {
