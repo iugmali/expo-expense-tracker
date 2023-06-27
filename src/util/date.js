76a02946f73dcbf67e416af7ab8e208b5ed9845c
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-06-27 16:35:58 -0300

    add new utility function for date formatting

--- src/util/date.js
+++ src/util/date.js
@@ -1,3 +1,7 @@
 export function getFormattedDate(date) {
   return `${date.getFullYear()}-${(date.getMonth() < 9) ? '0' : ''}${date.getMonth() + 1}-${(date.getDate() < 10) ? '0' : ''}${date.getDate()}`;
 }
+
+export function getDateMinusDays(date, days) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+}
