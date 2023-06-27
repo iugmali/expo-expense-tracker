c0b81ec6822a26cd4295ef24dede23d40a3f1fb7
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-06-27 16:36:48 -0300

    filter all expenses to the last 7 days

--- src/screens/RecentExpensesScreen.js
+++ src/screens/RecentExpensesScreen.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import {useSelector} from "react-redux";
+import {getDateMinusDays} from "../util/date";
 
 export default function RecentExpensesScreen() {
   const expenses = useSelector((state) => state.expenses.expenses);
+  const recentExpenses = expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+    return new Date(expense.date) > date7DaysAgo;
+  });
+
   return (
-    <ExpensesOutput expenses={expenses} expensesPeriod={'Last 7 days'} />
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod={'Last 7 days'} />
   );
 }
